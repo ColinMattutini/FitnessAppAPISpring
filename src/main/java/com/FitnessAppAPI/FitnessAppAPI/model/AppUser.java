a9631140de8306ee247c5182e7b195d0bd861049
Colin <cmmatt14@gmail.com>
2022-09-28 15:10:04 -0400

    updated appUser with attributes and lombok annotations

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/model/AppUser.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/model/AppUser.java
@@ -1,4 +1,33 @@
 package com.FitnessAppAPI.FitnessAppAPI.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
 public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+    private Collection<Role> roles = new ArrayList<>();
+
 }
