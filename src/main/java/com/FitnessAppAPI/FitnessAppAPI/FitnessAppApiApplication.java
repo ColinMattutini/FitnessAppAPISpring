c0950bdde385f424e01096424e383f93621f90ce
Colin <cmmatt14@gmail.com>
2022-09-27 21:57:35 -0400

    Initial Commit

---
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/FitnessAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.FitnessAppAPI.FitnessAppAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApiApplication.class, args);
+	}
+
+}
