8552eaf034aaedff64631d1b10fda340719e073d
Colin <cmmatt14@gmail.com>
2022-09-29 21:02:17 -0400

    began app service class

---
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/service/AppUserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.FitnessAppAPI.FitnessAppAPI.service;
+
+import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
+import com.FitnessAppAPI.FitnessAppAPI.model.Role;
+import com.FitnessAppAPI.FitnessAppAPI.repo.AppUserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
+
+    private final AppUserRepo appUserRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepo.findByUsername(username);
+        if(appUser == null){
+            log.error("Username {} not found", username);
+            throw new UsernameNotFoundException("User {} not found in database. Please check if the username is spelled correctly.");
+        } else {
+            log.info("User {} found in database successfully.", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        appUser.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(appUser.getUsername(), appUser.getPassword(), authorities);
+    }
+
+    @Override
+    public AppUser getAppUser(String username) {
+        return appUserRepo.findByUsername(username);
+    }
+
+    @Override
+    public AppUser saveAppUser(AppUser appUser) {
+        log.info("Saving user {} to the database", appUser.getUsername());
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+        String username = appUser.getUsername();
+        appUserRepo.save(appUser);
+
+        addRoleToAppUser(username, "ROLE_USER");
+        return appUser;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return null;
+    }
+
+    @Override
+    public void addRoleToAppUser(String username, String roleName) {
+
+    }
+
+
+}
