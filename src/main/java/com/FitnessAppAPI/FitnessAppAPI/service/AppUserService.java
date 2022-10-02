6cca8dc5037ba7aeb7d10e34f1eb3b99290c5249
Colin <cmmatt14@gmail.com>
2022-10-02 19:26:35 -0400

    added method to get all appusers

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/service/AppUserService.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/service/AppUserService.java
@@ -3,9 +3,12 @@ package com.FitnessAppAPI.FitnessAppAPI.service;
 import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
 import com.FitnessAppAPI.FitnessAppAPI.model.Role;
 
+import java.util.List;
+
 public interface AppUserService {
     AppUser getAppUser(String username);
     AppUser saveAppUser(AppUser appuser);
     Role saveRole(Role role);
+    List<AppUser> getAppUsers();
     void addRoleToAppUser(String username, String roleName);
 }
