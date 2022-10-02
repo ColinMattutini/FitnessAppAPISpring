b7812b8e5359161c6f20e306ef4f652fea8f45cf
Colin <cmmatt14@gmail.com>
2022-10-01 20:05:00 -0400

    added saving users to db

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/controller/AppUserController.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/controller/AppUserController.java
@@ -1,13 +1,27 @@
 package com.FitnessAppAPI.FitnessAppAPI.controller;
 
+import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
+import com.FitnessAppAPI.FitnessAppAPI.service.AppUserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class AppUserController {
 
+    private final AppUserService appUserService;
+
+    @GetMapping("/user/save")
+    public ResponseEntity<AppUser> saveAppUser(@RequestBody AppUser appUser){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(appUserService.saveAppUser(appUser));
+    }
 }
