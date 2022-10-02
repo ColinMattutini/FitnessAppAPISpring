da53f532f73f03d6e048ee74ee47738c745a2bb9
Colin <cmmatt14@gmail.com>
2022-10-02 15:39:21 -0400

    added securityfilterchain ao authorize requests

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/Security/SecurityConfig.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/Security/SecurityConfig.java
@@ -1,5 +1,30 @@
 package com.FitnessAppAPI.FitnessAppAPI.Security;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    @CrossOrigin(origins = "http://localhost:3000")
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/api/login/**", "/api/user/save");
+        http.authorizeRequests().anyRequest().authenticated();
+        return http.build();
+    }
 
 }
