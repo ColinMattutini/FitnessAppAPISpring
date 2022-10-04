2079844086afb8406603063c117075f0a80c3cc6
Colin <cmmatt14@gmail.com>
2022-10-03 20:21:58 -0400

    added api endpoint to get entries by username and some validation

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/controller/FoodEntryController.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/controller/FoodEntryController.java
@@ -2,22 +2,35 @@ package com.FitnessAppAPI.FitnessAppAPI.controller;
 
 import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
 import com.FitnessAppAPI.FitnessAppAPI.model.FoodEntry;
+import com.FitnessAppAPI.FitnessAppAPI.service.AppUserServiceImpl;
 import com.FitnessAppAPI.FitnessAppAPI.service.FoodEntryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class FoodEntryController {
-
-
     private final FoodEntryServiceImpl foodEntryService;
+    private final AppUserServiceImpl appUserService;
 
-    @PostMapping("/foodEntry")
-    public void addFoodEntry(@RequestBody FoodEntry foodEntry){ //, @PathVariable String username){
-        //foodEntryService.getFoodEntryByUsername(username);
-        foodEntryService.saveFoodEntry(foodEntry);
+    @PostMapping("/user/{username}/foodEntry")
+    public void addFoodEntry(@RequestBody FoodEntry foodEntry, @PathVariable String username){
+        if(appUserService.getAppUser(username).equals(null)){
+            throw new IllegalArgumentException("Username does not exist in Database.");
+        }else{
+            foodEntry.setUsername(username);
+            foodEntryService.saveFoodEntry(foodEntry);
+        }
+
+    }
+
+    @GetMapping("/user/{username}/foodEntry")
+    public List<FoodEntry> getFoodEntries(@PathVariable String username){
+        return foodEntryService.getFoodEntries(username);
     }
 }
