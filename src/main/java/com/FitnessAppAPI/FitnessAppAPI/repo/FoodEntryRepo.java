995c29c5514fda994597d933d892780c0a8bb1d6
Colin <cmmatt14@gmail.com>
2022-10-03 20:22:08 -0400

    added findbyusername

--- src/main/java/com/FitnessAppAPI/FitnessAppAPI/repo/FoodEntryRepo.java
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/repo/FoodEntryRepo.java
@@ -4,9 +4,11 @@ import com.FitnessAppAPI.FitnessAppAPI.model.FoodEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
+import java.util.List;
 
 public interface FoodEntryRepo extends JpaRepository<FoodEntry, Long> {
 
     FoodEntry findByDate(Date date);
+    List<FoodEntry> findByusername(String username);
 
 }
