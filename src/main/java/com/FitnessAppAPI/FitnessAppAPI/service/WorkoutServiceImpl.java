6c1fa002f08fac7ad884f2b19c3d4f7124ae8fb5
Colin <cmmatt14@gmail.com>
2022-10-16 20:45:28 -0400

    workout service implementation created

---
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/service/WorkoutServiceImpl.java
@@ -0,0 +1,47 @@
+package com.FitnessAppAPI.FitnessAppAPI.service;
+
+import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
+import com.FitnessAppAPI.FitnessAppAPI.model.FoodEntry;
+import com.FitnessAppAPI.FitnessAppAPI.model.Workout;
+import com.FitnessAppAPI.FitnessAppAPI.repo.WorkoutRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WorkoutServiceImpl implements WorkoutService{
+
+    @Autowired
+    WorkoutRepo workoutRepo;
+
+    @Override
+    public void saveWorkout(Workout workout) {
+        workoutRepo.save(workout);
+    }
+
+    @Override
+    public List<Workout> getAllWorkouts(AppUser appUser) {
+        List<Workout> workouts = new ArrayList<>();
+        workoutRepo.findByappUser(appUser).forEach(workouts::add);
+
+        return workouts;
+    }
+
+    @Override
+    public void deleteWorkout(Long id) {
+
+    }
+
+    @Override
+    public void updateWorkout(Workout workout, Long id) {
+
+    }
+
+    @Override
+    public Workout getWorkoutById(Long id) {
+        return workoutRepo.findByworkoutId(id);
+    }
+}
