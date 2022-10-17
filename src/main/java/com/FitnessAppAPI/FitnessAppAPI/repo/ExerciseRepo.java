646ee125d6f49b79afd164388a933edd012377d0
Colin <cmmatt14@gmail.com>
2022-10-16 20:46:45 -0400

    exercise repo created

---
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/repo/ExerciseRepo.java
@@ -0,0 +1,12 @@
+package com.FitnessAppAPI.FitnessAppAPI.repo;
+
+import com.FitnessAppAPI.FitnessAppAPI.model.Exercise;
+import com.FitnessAppAPI.FitnessAppAPI.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExerciseRepo extends JpaRepository<Exercise, Long> {
+    List<Exercise> findByWorkout(Workout workout);
+
+}
