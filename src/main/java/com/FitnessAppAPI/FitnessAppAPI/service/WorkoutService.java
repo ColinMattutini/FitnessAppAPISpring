e8296d96a48f1a92390e1d4d4e104a81fa44edd9
Colin <cmmatt14@gmail.com>
2022-10-16 20:45:17 -0400

    workout service interface  created

---
+++ src/main/java/com/FitnessAppAPI/FitnessAppAPI/service/WorkoutService.java
@@ -0,0 +1,17 @@
+package com.FitnessAppAPI.FitnessAppAPI.service;
+
+import com.FitnessAppAPI.FitnessAppAPI.model.AppUser;
+import com.FitnessAppAPI.FitnessAppAPI.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WorkoutService{
+
+    void saveWorkout(Workout workout);
+    List<Workout> getAllWorkouts(AppUser appUser);
+    void deleteWorkout(Long id);
+    void updateWorkout(Workout workout, Long id);
+
+    Workout getWorkoutById(Long id);
+}
